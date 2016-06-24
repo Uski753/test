c7227da391bfb977e544189e7ebd36b42585cedc
Uski753 <ErokhinAleksey@gmail.com>
2016-06-24 21:00:51 +0300

    Update funShift.js

--- funShift.js
+++ funShift.js
@@ -1,6 +1,5 @@
 window.onload=function (){
 document.body.addEventListener('click',function (ev){
-  console.log(ev);
   if (ev.shiftKey){
     if (ev.target.style.textTransform=='uppercase'){ev.target.style.textTransform='';} else {ev.target.style.textTransform='uppercase';};
       ev.preventDefault();
